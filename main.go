33c8d59888e4de5eebf9c9d85bdc66b92e3a83f1
eganbarov <ganbarov_emin@bk.ru>
2026-02-25 00:11:59 +0300

    Put handlers into handlep package and get redis lib

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/eganbarov/verification_code_service/handler"
 )
 
 func main() {
@@ -12,8 +14,8 @@ func main() {
 
 func startServer() {
 	mux := http.NewServeMux()
-	mux.Handle("POST /send-code", &SendCodeHandler{})
-	mux.Handle("POST /validate-code", &ValidateCodeHandler{})
+	mux.Handle("POST /send-code", &handler.SendCodeHandler{})
+	mux.Handle("POST /validate-code", &handler.ValidateCodeHandler{})
 
 	fmt.Println("Server starting on :8080")
 	if err := http.ListenAndServe(":8080", mux); err != nil {
